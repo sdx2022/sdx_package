5e6f88af503e321a71478fd3a2f68e6bc91488a0
Vannoch Sodex <sdxpm.pro@gmail.com>
2022-02-08 09:47:46 +0700

    laravel base code

---
+++ resources/views/abc.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ABC Testing</title>
+</head>
+<body>
+	<h1>ABC Testing</h1>
+</body>
+</html>
